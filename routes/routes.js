aa3d728206d49ddfdf801ed7e5a301ed26686e97
vinaykakkad <56934712+vinaykakkad@users.noreply.github.com>
2022-10-16 03:22:08 +0530

    format: formatted using prettier

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const authMiddleware = require("../middleware/auth.middleware");
 const router = express.Router();
 
-router.use("/auth", require('./auth.route'));
+router.use("/auth", require("./auth.route"));
 
 module.exports = router;
